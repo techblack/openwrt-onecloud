cce380167bb489744e77d2e185c6b1465e072413
shiyu1314 <shiyu1052@gmail.com>
2024-07-07 12:59:56 +0800

    op

---
+++ sh/im.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+
+echo 'src-git dns https://github.com/sbwml/luci-app-mosdns' >>feeds.conf.default
+echo 'src-git xd https://github.com/shiyu1314/openwrt-packages' >>feeds.conf.default
+git clone -b master --depth 1 --single-branch https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+
+
+./scripts/feeds update -a
+rm -rf feeds/packages/net/mosdns
+rm -rf feeds/packages/net/v2ray-geodata
+
+./scripts/feeds update -a
+./scripts/feeds install -a
+
+sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
+
+sudo rm -rf package/base-files/files/etc/banner
+
+sed -i "s/%D %V %C/%D $(TZ=UTC-8 date +%Y.%m.%d)/" package/base-files/files/etc/openwrt_release
+
+sed -i "s/%R/by $OP_author/" package/base-files/files/etc/openwrt_release
+
+
+date=$(date +"%Y-%m-%d")
+echo "                                                    " >> package/base-files/files/etc/banner
+echo ".___                               __         .__" >> package/base-files/files/etc/banner
+echo "|   | _____   _____   ____________/  |______  |  |" >> package/base-files/files/etc/banner
+echo "|   |/     \ /     \ /  _ \_  __ \   __\__  \ |  |" >> package/base-files/files/etc/banner
+echo "|   |  Y Y  \  Y Y  (  <_> )  | \/|  |  / __ \|  |__" >> package/base-files/files/etc/banner
+echo "|___|__|_|  /__|_|  /\____/|__|   |__| (____  /____/" >> package/base-files/files/etc/banner
+echo "          \/      \/                        \/      " >> package/base-files/files/etc/banner
+echo " -----------------------------------------------------" >> package/base-files/files/etc/banner
+echo "         %D ${date} by $OP_author                     " >> package/base-files/files/etc/banner
+echo " -----------------------------------------------------" >> package/base-files/files/etc/banner
+echo "                                                      " >> package/base-files/files/etc/banner
